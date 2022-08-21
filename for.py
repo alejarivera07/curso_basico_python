e08045f0683286226df3d53dbd809d3d732ddc8b
Alejandra Rivera <alejandra.rivera@frubana.com>
2022-08-21 09:38:02 -0500

    examples cicle for

---
+++ for.py
@@ -0,0 +1,11 @@
+# contador = 1
+# print(contador)
+# while contador < 1000:
+#     contador += contador 
+#     print(contador)
+
+# a = list(range(1000))
+# print(a)
+
+for contador in range(1, 1001):
+    print(contador)
