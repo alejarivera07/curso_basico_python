e64cfe769dcd4f326b281f5530277fcca76ab0da
Alejandra Rivera <alejandra.rivera@frubana.com>
2022-08-21 09:08:27 -0500

    function to create powers of two

--- bucles.py
+++ bucles.py
@@ -1,5 +1,18 @@
 
-for numero in range(1,11):
-    print(2**numero)
+# for numero in range(1,11):
+#     print(2**numero)
 
-## haré un comentario por acá
+# ## haré un comentario por acá
+
+def run():
+    LIMITE = 1000
+
+    contador = 0
+    potencia_2 = 2**contador
+    while potencia_2 < LIMITE:
+        print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))
+        contador = contador + 1
+        potencia_2 = 2**contador
+
+if __name__ == '__main__':
+    run()
